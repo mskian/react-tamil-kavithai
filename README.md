c19c3a17b1d57e1c5903822a7ce2615af0dc9026
mskian <veerfun23@gmail.com>
2021-01-21 18:45:54 +0530

    First commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# Tamil kavithai ☀
+
+![build-test](https://github.com/mskian/react-tamil-kavithai/workflows/build-test/badge.svg)  
+
+Tamil SMS Blog - Get Random Tamil kavithai and Tamil Quotes 💗
+
+⚛ React | ⚡ Speed | ☁ Offline | 🌩 PWA
+
+## Features 🍔
+
+- React JS
+- PWA
+- Offline Suppport
+- Bulma CSS
+- Power Packed with React Modules
+
+## Installation 📦
+
+- Clone this repo or Download
+
+```sh
+git clone https://github.com/mskian/react-tamil-kavithai.git
+cd react-tamil-kavithai
+yarn install
+```
+
+- Test the site
+
+```sh
+yarn start
+```
+
+- Build the site
+
+```sh
+yarn build
+```
+
+- Test the production Build on Localhost
+
+```sh
+npm install -g serve
+yarn server
+```
+
+- Production Build avilable in the folder `build`
+
+## LICENSE ☑
+
+MIT
---
+++ src/App.js
@@ -0,0 +1,94 @@
+import React, { Component } from 'react';
+import 'bulma/css/bulma.min.css'
+import './index.css';
+import axios from "axios";
+import { Fade } from "react-awesome-reveal";
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import {CopyToClipboard} from 'react-copy-to-clipboard';
+
+class RetrieveList extends Component {
+ 
+  constructor(props) {
+     super(props)
+   this.state = {
+       isLoading: true,
+       persons: []
+    }
+ };
+ componentDidMount() {
+    this.getQuote()
+ }
+getQuote() {
+  toast.dark('🦄 Kavithai Updated', {
+    position: "top-right",
+    autoClose: 800,
+    hideProgressBar: true,
+    closeOnClick: true,
+    pauseOnHover: true,
+    draggable: true,
+    progress: undefined
+    });
+    axios.get("https://app.santhoshveer.com/san.php")
+      .then(res=>{
+        const tamilsms = res.data;
+        this.setState({ isLoading: false, persons: tamilsms});
+      })
+  }
+  Copykavithai() {
+    toast.dark('🦄 Kavithai Copied', {
+      position: "top-right",
+      autoClose: 800,
+      hideProgressBar: true,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined
+      });
+    }
+getNewQuote = () => {
+    this.getQuote()
+ }
+  render() {
+    return (
+        <section className="section">
+        <div className="container content">
+        <div className="columns is-centered">
+        <div className="column is-half">
+        <br />
+        <h1 className="title has-text-warning has-text-centered">
+         💗 Tamil Kavithai 💗
+        </h1>
+        {this.state.isLoading ? "" : 
+        <div className="notification has-text-weight-bold read-more">
+        {this.state.persons.map(person=><p key = {person.id} dangerouslySetInnerHTML={{__html: person.content}}></p>)}
+        </div>
+        }
+        <div className="buttons is-centered">
+        <button className="button is-warning read-random" onClick={this.getNewQuote} disabled={this.state.isLoading}>{this.state.isLoading ? "Loading..." : "🔄 Random kavithai"}</button>
+        <ToastContainer />
+        <CopyToClipboard text={this.state.persons.map(person => (person.content).replace(/<br \/>/g, "\n"))} onCopy={() => this.setState({copied: true})}>
+        <button className="button is-link read-random" onClick={this.Copykavithai} disabled={this.state.isLoading}>{this.state.isLoading ? "Loading..." : "📝 Copy kavithai"}</button>
+        </CopyToClipboard>
+        </div>
+        <br />
+        <br />
+        <hr />
+        <Fade>
+        <p className="has-text-success has-text-weight-bold has-text-centered">
+          தமிழ் எஸ் எம் எஸ் 💌 <br />
+          PWA Web App from the Creator of tamilsms.blog - Built using React JS <br />
+          <br />
+          <img src="./react.png" loading="lazy" height="45" width="45" alt="Tamil SMS" />
+        </p>
+        </Fade>
+        <br />
+        <br />
+        </div>
+        </div>
+        </div>
+        </section> 
+    );
+  }
+}
+export default RetrieveList;
